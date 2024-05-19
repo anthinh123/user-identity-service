71a8fd24414136b5643e48a77265a28e15fb4197
thinh <anvanthinh@gmail.com>
2024-05-19 18:01:00 +0700

    init user identity service

---
+++ src/test/java/com/thinh/useridentityservice/UseridentityserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinh.useridentityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseridentityserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
