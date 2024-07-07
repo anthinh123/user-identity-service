09edf6f8c9b9fe6cf24716bcb35f19c4a38b89ad
thinh <anvanthinh@gmail.com>
2024-07-07 22:51:48 +0700

    update config

--- src/main/java/com/thinh/useridentityservice/UserIdentityServiceApplication.java
+++ src/main/java/com/thinh/useridentityservice/UserIdentityServiceApplication.java
@@ -2,8 +2,10 @@ package com.thinh.useridentityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserIdentityServiceApplication {
 
 	public static void main(String[] args) {
